cb712735bd46db53f1d32f9ea3a0b00dbda34400
karasu <karasu@antergos.com>
2017-09-04 16:35:12 +0200

    Unified everything in just one file.

--- downgrade-linux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-yad --title "Kernels" --buttons-layout=center --skip-taskbar --center --text="To properly downgrade a linux kernel, one must downgrade the kernel itself and its headers to the same version" \
---button="Downgrade linux":"xterm -e downgrade linux" \
---button="Downgrade linux-headers":"xterm -e downgrade linux-headers" \
--- kernels3.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-yad --title "Kernels" --height=100 --width=300 --center --buttons-layout=center --text="An Easy Kernel Manager For Everyone" --text-align=center \
---button="Install":"kernels.sh" \
---button="Remove":"kernels2.sh" \
---button="Downgrade":"kernels4.sh" \
